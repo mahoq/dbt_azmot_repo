37641682f40e6bf128bd940d1b58f2029ce41341
azmot ali <azmot@drms.com.bd>
2024-12-15 10:21:18 +0000

    ok

--- models/test_query.sql
+++ models/test_query.sql
@@ -1,6 +1,16 @@
 with 
-test_sql_query as (
-    select name, platform, year_
-    from product
+    test_sql_query as (
+        select name, platform, year_
+        from prod.product
 )
 select * from test_sql_query
+
+
+--with
+--    product_sales_hoq_roselands as (
+--        select city, state
+--        from prod.product
+--        group by 1, 2
+--    )
+--select *
+--    from product_sales_hoq_roselands
