3aaf73513582559f09faa44ec17e61b7c3a1b45e
azmot ali <azmot@drms.com.bd>
2024-12-15 13:36:30 +0000

    ok

--- models/Training/store_sales_tbl_2023.sql
+++ models/Training/store_sales_tbl_2023.sql
@@ -1,6 +1,6 @@
 {{ config(materialized='table') }}
 with Store_Sales_tbl_2023 as
 (
-    select ss_store_sk, ss_quantity, ss_sales_price, ss_net_profit from prod.STORE_Sales_tbl_2023
+    select ss_store_sk, ss_quantity, ss_sales_price, ss_net_profit from prod.STORE_Sales_2023
 )
 select * from Store_Sales_tbl_2023
