6a29ec85100383b8f33b818613af7f84396a6cdf
azmot ali <azmot@drms.com.bd>
2024-12-16 04:16:12 +0000

    finally done, where a comma was missed for the last cte

--- models/stud_prac/stud_cte_final.sql
+++ models/stud_prac/stud_cte_final.sql
@@ -1,17 +1,12 @@
-with stud_cte_final_1 as
-(
-    select * from {{ ref('stud_cte_tbl1') }}
-),
-stud_cte_final_2 as
-(
-    select * from {{ ref('stud_cte_tbl2') }}
-)
-
-Stud_cte_final as
-(
-    --select sid, f_name from stud_cte_final_1
-    select l_name, fee_amount, class_in from stud_cte_final_1, stud_cte_final_2
-    where stud_cte_final_1.sid = stud_cte_final_2.sid
-    group by 1
-)
-select * from Stud_cte_final
+with
+    stud_cte_final_1 as (select * from {{ ref("stud_cte_tbl1") }}),
+    stud_cte_final_2 as (select * from {{ ref("stud_cte_tbl2") }}),
+    stud_cte_final as (
+        -- select sid, f_name from stud_cte_final_1
+        select l_name, fee_amount, class_in
+        from stud_cte_final_1, stud_cte_final_2
+        where stud_cte_final_1.sid = stud_cte_final_2.sid
+        group by 1
+    )
+select *
+from stud_cte_final
