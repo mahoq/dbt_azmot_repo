6d6611d7da6e5d380fdd991ca2cdf81b14f25c15
azmot ali <azmot@drms.com.bd>
2024-12-16 04:06:03 +0000

    ok

--- models/stud_prac/stud_cte_final.sql
+++ models/stud_prac/stud_cte_final.sql
@@ -7,10 +7,11 @@ stud_cte_final_2 as
     select * from {{ ref('stud_cte_tbl2') }}
 )
 
-stud_cte_final as
+Stud_cte_final as
 (
-    select sid, f_name, fee, class from stud_cte_final_1, stud_cte_final_2
-    where sid=sid
+    --select sid, f_name from stud_cte_final_1
+    select l_name, fee_amount, class_in from stud_cte_final_1, stud_cte_final_2
+    where stud_cte_final_1.sid = stud_cte_final_2.sid
     group by 1
 )
-select * from stud_cte_final
+select * from Stud_cte_final
