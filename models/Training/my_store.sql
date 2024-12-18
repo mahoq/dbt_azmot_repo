68900d82c2a54137db639a8f8ff4ca4fb0917346
azmot ali <azmot@drms.com.bd>
2024-12-18 19:20:40 +0000

    ok

---
+++ models/Training/my_store.sql
@@ -0,0 +1,6 @@
+{{ config(materialized='table') }}
+with store_Sales_tbl_2023 as
+(
+    select S_STORE_SK, S_STORE_ID, S_STORE_NAME, S_COUNTY  from {{ source('test_prod', 'Store_2023')}}
+)
+select * from store_Sales_tbl_2023
