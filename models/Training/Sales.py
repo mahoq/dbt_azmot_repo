0cf33293d1f90ff5d1746eeb1789df412e5ce352
mahoq <mahoq@users.noreply.github.com>
2024-12-18 18:19:10 +1100

    Update Sales.py
    ok

--- models/Training/Sales.py
+++ models/Training/Sales.py
@@ -2,6 +2,6 @@ import snowflake.snowpark.functions as F
 def model (dbt,session):
   df = session.sql("select * from prod.product")
   df_filter= df.filter(F.col("REGION")=='West')
-  df_final = df_filter.group_by("STATE").agg(F.sum(F.col('GLOBAL_SALES').alias('Total Global Sales')))
+  df_final = df_filter.group_by("STATE").agg(F.sum(F.col('GLOBAL_SALES').alias('Total Test Sales Price')))
 
   return df_final
