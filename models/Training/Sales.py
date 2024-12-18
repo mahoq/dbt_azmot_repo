5c0024117df653144b66e2c27307444b700285d5
azmot ali <azmot@drms.com.bd>
2024-12-18 07:17:40 +0000

    ok

--- models/Training/Sales.py
+++ models/Training/Sales.py
@@ -2,6 +2,6 @@ import snowflake.snowpark.functions as F
 def model (dbt,session):
   df = session.sql("select * from prod.product")
   df_filter= df.filter(F.col("REGION")=='West')
-  df_final = df_filter.group_by("STATE").agg(F.sum(F.col('GLOBAL_SALES')))
+  df_final = df_filter.group_by("STATE").agg(F.sum(F.col('GLOBAL_SALES').alias('Total Global Sales')))
 
   return df_final
