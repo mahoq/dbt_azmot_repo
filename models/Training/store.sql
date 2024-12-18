bcaba77d3e427187b10167b945f4cc791a43c7a2
azmot ali <azmot@drms.com.bd>
2024-12-18 08:10:08 +0000

    ok

--- models/Training/store.sql
+++ models/Training/store.sql
@@ -1,8 +1,10 @@
- {{ config(materialized='table') }}
-with store_tbl_2023 as
-(
-  --select S_STORE_SK,S_STORE_ID,S_STORE_NAME,S_COUNTY from Prod.Store_2023 
-  -- {{ source('test', 'Store_2023')}}
-  select S_STORE_SK,S_STORE_ID,S_STORE_NAME,S_COUNTY from {{ source('test_prod', 'Store_2023') }}
-)
-select * from store_tbl_2023
+{{ config(materialized="table") }}
+with
+    store_tbl_2023 as (
+        --select s_store_sk, s_store_id, s_store_name, s_county
+        --from {{ source("test_prod", "Store_2023") }}
+        select s_store_sk, s_store_id, s_store_name, s_county
+        from dbt_db.prod.Store_2023
+    )
+select *
+from store_tbl_2023
