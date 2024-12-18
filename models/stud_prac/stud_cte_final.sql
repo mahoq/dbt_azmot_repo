2702260a026489545bd337418d668ee3c47ea2e3
azmot ali <azmot@drms.com.bd>
2024-12-18 06:01:27 +0000

    added this .py file for python code test in dbt

--- models/stud_prac/stud_cte_final.sql
+++ models/stud_prac/stud_cte_final.sql
@@ -1,9 +1,10 @@
+{{ config(materialized='table')}}
 with
     stud_cte_final_1 as (select * from {{ ref("stud_cte_tbl1") }}),
     stud_cte_final_2 as (select * from {{ ref("stud_cte_tbl2") }}),
     stud_cte_final as (
         -- select sid, f_name from stud_cte_final_1
-        select l_name, fee_amount, class_in
+        select l_name, sum(fee_amount)
         from stud_cte_final_1, stud_cte_final_2
         where stud_cte_final_1.sid = stud_cte_final_2.sid
         group by 1
